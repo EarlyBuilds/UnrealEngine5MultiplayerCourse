b1b88afdb3bd8abd03ce413419e6477dbd0ef134
EarlyBuilds <102487692+EarlyBuilds@users.noreply.github.com>
2022-10-25 19:50:46 +0200

    learning the basics

--- CourseShooter/Source/CourseShooter/ShooterCharacter.cpp
+++ CourseShooter/Source/CourseShooter/ShooterCharacter.cpp
@@ -16,10 +16,33 @@ void AShooterCharacter::BeginPlay()
 {
 	Super::BeginPlay();
 	UE_LOG(LogTemp, Warning, TEXT("Begin Play"));
+	//setting and printing a int
 	int myInt{ 42 };
 	UE_LOG(LogTemp, Warning, TEXT("int myInt: %d"), myInt);
+	//setting a printing a float
 	float myFloat{ 3.14159f };
-	UE_LOG(LogTemp, Warning, TEXT("int myFloat: %d"), myFloat);
+	UE_LOG(LogTemp, Warning, TEXT("float myFloat: %f"), myFloat);
+	//setting and printing a double
+	double myDouble{ 0.000756 };
+	UE_LOG(LogTemp, Warning, TEXT("double myDouble: %lf"), myDouble);
+	//setting and print a char
+	char myChar('J');
+	UE_LOG(LogTemp, Warning, TEXT("char myChar: %c"), myChar);
+	//setting and print a widechar
+	wchar_t wideChar{L'J'};
+	UE_LOG(LogTemp, Warning, TEXT("wchar_t wideChar: %lc"), wideChar);
+	//setting and printing a boolean
+	bool myBool{true};
+	UE_LOG(LogTemp, Warning, TEXT("bool myBool: %d"), myBool);
+	//printing multiple things at once
+	UE_LOG(LogTemp, Warning, TEXT("int: %d, float: %f, bool: %d"), myInt, myFloat, myBool);
+	//setting and printing a string
+	FString myString{ TEXT("My first c++ string!") };
+	UE_LOG(LogTemp, Warning, TEXT("FString myString: %s"), *myString);
+
+	//get name of the current object
+	UE_LOG(LogTemp, Warning, TEXT("Name of instance: %s"), *GetName());
+
 }
 
 // Called every frame
