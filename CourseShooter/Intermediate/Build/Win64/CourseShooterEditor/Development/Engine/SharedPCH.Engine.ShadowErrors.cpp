7cd3ef9868e6e1871d56255936aeda3ca1096249
EarlyBuilds <102487692+EarlyBuilds@users.noreply.github.com>
2022-10-25 19:18:11 +0200

    2nd commit

--- CourseShooter/Intermediate/Build/Win64/CourseShooterEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
+++ CourseShooter/Intermediate/Build/Win64/CourseShooterEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -1,2 +1,2 @@
 // Compiler: 14.33.31630
-#include "E:/projects/UE5CourseMP/CourseShooter/Intermediate/Build/Win64/CourseShooterEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
+#include "E:/projects/UnrealEngine5MultiplayerCourse/CourseShooter/Intermediate/Build/Win64/CourseShooterEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
--- CourseShooter/Source/CourseShooter/ShooterCharacter.cpp
+++ CourseShooter/Source/CourseShooter/ShooterCharacter.cpp
@@ -18,6 +18,8 @@ void AShooterCharacter::BeginPlay()
 	UE_LOG(LogTemp, Warning, TEXT("Begin Play"));
 	int myInt{ 42 };
 	UE_LOG(LogTemp, Warning, TEXT("int myInt: %d"), myInt);
+	float myFloat{ 3.14159f };
+	UE_LOG(LogTemp, Warning, TEXT("int myFloat: %d"), myFloat);
 }
 
 // Called every frame
