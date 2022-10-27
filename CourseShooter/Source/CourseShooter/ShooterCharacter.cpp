0e747bf87c976ce68b985c426d0d62c3b94f7fe2
EarlyBuilds <102487692+EarlyBuilds@users.noreply.github.com>
2022-10-27 22:25:52 +0200

    Commit4
    added movement!

--- CourseShooter/Source/CourseShooter/ShooterCharacter.cpp
+++ CourseShooter/Source/CourseShooter/ShooterCharacter.cpp
@@ -65,6 +65,28 @@ void AShooterCharacter::BeginPlay()
 
 }
 
+void AShooterCharacter::MoveForward(float Value)
+{
+	if ((Controller != nullptr) && (Value != 0.0f)) {
+		// code to find the forward direction
+		const FRotator Rotation{ Controller->GetControlRotation() };
+		const FRotator YawRotation{ 0,Rotation.Yaw,0 };
+		const FVector Direction{ FRotationMatrix{YawRotation}.GetUnitAxis(EAxis::X)};
+		AddMovementInput(Direction, Value);
+	}
+}
+
+void AShooterCharacter::MoveRight(float Value)
+{
+	if ((Controller != nullptr) && (Value != 0.0f)) {
+		// code to find the right direction
+		const FRotator Rotation{ Controller->GetControlRotation() };
+		const FRotator YawRotation{ 0,Rotation.Yaw,0 };
+		const FVector Direction{ FRotationMatrix{YawRotation}.GetUnitAxis(EAxis::Y) };
+		AddMovementInput(Direction, Value);
+	}
+}
+
 // Called every frame
 void AShooterCharacter::Tick(float DeltaTime)
 {
@@ -76,6 +98,8 @@ void AShooterCharacter::Tick(float DeltaTime)
 void AShooterCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
-
+	check(PlayerInputComponent);
+	PlayerInputComponent->BindAxis("MoveForward", this, &AShooterCharacter::MoveForward);
+	PlayerInputComponent->BindAxis("MoveRight", this, &AShooterCharacter::MoveRight);
 }
 
